30907017f6569165dac7a012cfc581b6ab23053b
Jagger81 <eddie.cranmer@concern.net>
2017-07-11 08:39:28 +0100

    JQuery Stuff

---
+++ documents/code institute/Stream 1/files-l18/Sample Site/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+	
+	$("tr:even").addClass("rowcolorEven");
+
+    $("th:even").addClass("rowcolorEven");     
+	
+	$("tr:odd").addClass("rowcolorOdd");
+
+});
+}); 
---
+++ documents/code institute/Stream 1/files-l18/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ 
+	$("tr:even").addClass("rowcolorEven");
+
+        
+	
+	$("tr:odd").addClass("rowcolorOdd");
+	
+	
+
+}); 
